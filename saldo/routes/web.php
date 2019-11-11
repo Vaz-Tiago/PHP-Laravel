f2c381871008588a4f46b0476590a373abc4c1d0
Tiago Vaz <55505983+Vaz-Tiago@users.noreply.github.com>
2019-11-11 16:45:23 -0300

    Organização Rotas e Controllers
    Feita a organização das rotas e restrição de acesso à área de admin.

--- saldo/routes/web.php
+++ saldo/routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+//Grupo filtrado pelo middleware de autenticação
+//namespace => Admin é para não ter que ficar esecificando a pasta que esta o controller, pois vai buscar todos nesta pasta
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin'], function () {
+    Route::get('admin', ['as' => 'Admin.Home', 'uses' => 'AdminController@Index']);
+});
+
+
+
 Route::get('/', ['as' => 'Site.Index', 'uses' => 'Site\SiteController@Index']);
 
 Auth::routes();
-
-Route::get('/home', function() {
-    return view('home');
-})->name('home')->middleware('auth');
